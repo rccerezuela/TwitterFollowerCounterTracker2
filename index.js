3639dc6b76cdff3a46bb34f16553bb30e584f570
r0b3rt0 <rc.cerezuela@raraavislab.es>
2020-08-22 16:23:51 +0000

    Get followers el corte ingles and print in terminal

--- index.js
+++ index.js
@@ -1,68 +1,33 @@
-var express = require('express');
-var sys = require('sys');
-var oauth = require('oauth');
+const OAuth = require('oauth')
+const got = require('got')
+const { promisify } = require('util')
 
-var app = express.createServer();
 
-var _twitterConsumerKey = "8AwdDjAbUCw5FtYG1YrVLvi3o";
-var _twitterConsumerSecret = "pYOwSP8vWzxDP4iHsciu7twwOw8HFSEL560J4LOsxjTYnxATon";
 
-function consumer() {
-  return new oauth.OAuth(
-    "https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token", 
-    _twitterConsumerKey, _twitterConsumerSecret, "1.0A", "https://raraavislab.es", "HMAC-SHA1");   
+getTwitterUserProfileWithOAuth2('elcorteingles')
+    .then((profile) => 
+      showResult(profile)
+    )
+    .catch(err => console.error(err) && process.exit(1))
+
+async function getTwitterUserProfileWithOAuth2 (username = 'elcorteingles') {
+  var oauth2 = new OAuth.OAuth2(
+    "8AwdDjAbUCw5FtYG1YrVLvi3o",
+    "pYOwSP8vWzxDP4iHsciu7twwOw8HFSEL560J4LOsxjTYnxATon",
+    'https://api.twitter.com/', null, 'oauth2/token', null
+  )
+  const getOAuthAccessToken = promisify(oauth2.getOAuthAccessToken.bind(oauth2))
+  const accessToken = await getOAuthAccessToken('', { grant_type: 'client_credentials' })
+
+  return got(`https://api.twitter.com/1.1/users/show.json?screen_name=${username}`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  })
+    .then((res) => JSON.parse(res.body))
 }
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  app.use(express.logger());
-  app.use(express.cookieDecoder());
-  app.use(express.session());
-});
-
-app.dynamicHelpers({
-  session: function(req, res){
-    return req.session;
-  }
-});
-
-app.get('/', function(req, res){
-  res.send('Hello World');
-});
-
-app.get('/sessions/connect', function(req, res){
-  consumer().getOAuthRequestToken(function(error, oauthToken, oauthTokenSecret, results){
-    if (error) {
-      res.send("Error getting OAuth request token : " + sys.inspect(error), 500);
-    } else {  
-      req.session.oauthRequestToken = oauthToken;
-      req.session.oauthRequestTokenSecret = oauthTokenSecret;
-      res.redirect("https://twitter.com/oauth/authorize?oauth_token="+req.session.oauthRequestToken);      
-    }
-  });
-});
-
-app.get('/sessions/callback', function(req, res){
-  sys.puts(">>"+req.session.oauthRequestToken);
-  sys.puts(">>"+req.session.oauthRequestTokenSecret);
-  sys.puts(">>"+req.query.oauth_verifier);
-  consumer().getOAuthAccessToken(req.session.oauthRequestToken, req.session.oauthRequestTokenSecret, req.query.oauth_verifier, function(error, oauthAccessToken, oauthAccessTokenSecret, results) {
-    if (error) {
-      res.send("Error getting OAuth access token : " + sys.inspect(error) + "["+oauthAccessToken+"]"+ "["+oauthAccessTokenSecret+"]"+ "["+sys.inspect(results)+"]", 500);
-    } else {
-      req.session.oauthAccessToken = oauthAccessToken;
-      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
-      // Right here is where we would write out some nice user stuff
-      consumer.get("http://twitter.com/account/verify_credentials.json", req.session.oauthAccessToken, req.session.oauthAccessTokenSecret, function (error, data, response) {
-        if (error) {
-          res.send("Error getting twitter screen name : " + sys.inspect(error), 500);
-        } else {
-          req.session.twitterScreenName = data["screen_name"];    
-          res.send('You are signed in: ' + req.session.twitterScreenName)
-        }  
-      });  
-    }
-  });
-});
-
-app.listen(parseInt(process.env.PORT || 80));
+async function showResult(result){
+  console.log('Followers el corte ingles', result.followers_count);
+  process.exit(0);
+}
