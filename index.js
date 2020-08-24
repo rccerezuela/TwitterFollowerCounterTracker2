7206e905ec55d91f92a6039b42dbe05d763afbc3
r0b3rt0 <rc.cerezuela@raraavislab.es>
2020-08-24 21:58:43 +0000

    persist measured followers in firebase

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const OAuth = require('oauth')
 const got = require('got')
 const { promisify } = require('util')
+const firebase = require("firebase");
+
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCgduBXRzvbX-ThWP_R5t_xcxYEMPqVzjc",
+    authDomain: "twitterfollowercountertracker2.firebaseapp.com",
+    databaseURL: "https://twitterfollowercountertracker2.firebaseio.com",
+    projectId: "twitterfollowercountertracker2",
+    storageBucket: "twitterfollowercountertracker2.appspot.com",
+    messagingSenderId: "978600642574",
+    appId: "1:978600642574:web:e02cd7a2839e83320d945f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 
 
@@ -10,12 +24,6 @@ getTwitterUserProfileWithOAuth2('elcorteingles')
     )
     .catch(err => console.error(err) && process.exit(1))
 
-
-getTwitterUserProfileWithOAuth2('elcorteingles')
-    .then((profile) => 
-      showIdResult(profile)
-    )
-    .catch(err => console.error(err) && process.exit(1))
 
 async function getTwitterUserProfileWithOAuth2 (username = 'elcorteingles') {
   var oauth2 = new OAuth.OAuth2(
@@ -35,11 +43,29 @@ async function getTwitterUserProfileWithOAuth2 (username = 'elcorteingles') {
 }
 
 async function showFollowersResult(result){
-  console.log('Followers el corte ingles', result.followers_count);
+
+  const fireBaseRegister = {
+    "twitter_id":  result.id,
+    "followers":   result.followers_count,
+    "twitter_name": "elcorteingles",
+    "data_date": Date.now()
+  }
+
+  const database = firebase.database();
+  let dataRef = database.ref('measures');
+  let dataPush = dataRef.push(fireBaseRegister);
+
+  dataRef.once('value', snapshot => {
+  console.log(snapshot.val());
+});
+
+/*
+const date =  fireBaseRegister.data_date;
+const dateTimeFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short', day: '2-digit' }) 
+const [{ value: month },,{ value: day },,{ value: year }] = dateTimeFormat .formatToParts(date ) 
+
+console.log(`${day}-${month}-${year }`)
+*/
+
  
-}
-
-async function showIdResult(result){
-  console.log('Id user el corte ingles', result.id);
-
 }
