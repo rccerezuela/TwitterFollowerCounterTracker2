8474ffdc9cd53852eec6c63a37875fe9dc17bf8d
r0b3rt0 <rc.cerezuela@raraavislab.es>
2020-10-30 08:17:19 +0000

    Web accesible code

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
+const express = require('express');
 const OAuth = require('oauth')
 const got = require('got')
-const {promisify} = require('util')
+const { promisify } = require('util')
 const firebase = require("firebase");
 
+const app = express();
 
-var firebaseConfig = {
-  apiKey: process.env['FIREBASE_API_KEY'],
-  authDomain: "twitterfollowercountertracker2.firebaseapp.com",
-  databaseURL: "https://twitterfollowercountertracker2.firebaseio.com",
-  projectId: "twitterfollowercountertracker2",
-  storageBucket: "twitterfollowercountertracker2.appspot.com",
-  messagingSenderId: "978600642574",
-  appId: "1:978600642574:web:e02cd7a2839e83320d945f"
-};
+app.get('/', (req, res) => {
 
-firebase.initializeApp(firebaseConfig);
+  var firebaseConfig = {
+    apiKey: process.env['FIREBASE_API_KEY'],
+    authDomain: "twitterfollowercountertracker2.firebaseapp.com",
+    databaseURL: "https://twitterfollowercountertracker2.firebaseio.com",
+    projectId: "twitterfollowercountertracker2",
+    storageBucket: "twitterfollowercountertracker2.appspot.com",
+    messagingSenderId: "978600642574",
+    appId: "1:978600642574:web:e02cd7a2839e83320d945f"
+  };
 
-
-getTwitterUserProfileWithOAuth2('elcorteingles')
-  .then((profile) => showFollowersResult(profile) )
-  .catch(err => console.error(err) && process.exit(1))
+  firebase.initializeApp(firebaseConfig);
 
 
-async function getTwitterUserProfileWithOAuth2 (username = 'elcorteingles') {
-  var oauth2 = new OAuth.OAuth2(
-    process.env['TWITTER_KEY'],
-    process.env['TWITTER_SECRET'],
-    'https://api.twitter.com/', null, 'oauth2/token', null
-  )
-  const getOAuthAccessToken = promisify(oauth2.getOAuthAccessToken.bind(oauth2))
-  const accessToken = await getOAuthAccessToken('', { grant_type: 'client_credentials' })
+  getTwitterUserProfileWithOAuth2('elcorteingles')
+    .then((profile) => showFollowersResult(profile))
+    .catch(err => console.error(err) && process.exit(1))
 
-  return got(`https://api.twitter.com/1.1/users/show.json?screen_name=${username}`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-    .then((res) => JSON.parse(res.body))
-}
 
-async function showFollowersResult(result){
+  async function getTwitterUserProfileWithOAuth2(username = 'elcorteingles') {
+    var oauth2 = new OAuth.OAuth2(
+      process.env['TWITTER_KEY'],
+      process.env['TWITTER_SECRET'],
+      'https://api.twitter.com/', null, 'oauth2/token', null
+    )
+    const getOAuthAccessToken = promisify(oauth2.getOAuthAccessToken.bind(oauth2))
+    const accessToken = await getOAuthAccessToken('', { grant_type: 'client_credentials' })
 
-  const fireBaseRegister = {
-    "twitter_id":  result.id,
-    "followers":   result.followers_count,
-    "twitter_name": "elcorteingles",
-    "data_date": Date.now()
+    return got(`https://api.twitter.com/1.1/users/show.json?screen_name=${username}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+      .then((res) => JSON.parse(res.body))
   }
-  
-  const database = firebase.database();
-  let dataRef = database.ref('measures');
-  let dataPush = await dataRef.push(fireBaseRegister); 
 
-  console.log(fireBaseRegister);
-}
+  async function showFollowersResult(result) {
+
+    const fireBaseRegister = {
+      "twitter_id": result.id,
+      "followers": result.followers_count,
+      "twitter_name": "elcorteingles",
+      "data_date": Date.now()
+    }
+
+    const database = firebase.database();
+    let dataRef = database.ref('measures');
+    let dataPush = await dataRef.push(fireBaseRegister);
+
+    console.log(fireBaseRegister);
+  }
+
+  res.send('Hello Express app!')
+});
+
+
